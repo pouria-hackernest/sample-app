37badb0f88bafb7989cfa0b27f81035c93d3b582
Pouria Mashouri <pouria@hackernest.com>
2015-08-17 01:42:33 -0400

    Users working now

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,75 @@
+class UsersController < ApplicationController
+  
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+      sign_in @user
+  		flash[:success] = "Welcome to the Sample App!"
+		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #handle update
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    deletedUser = User.find(params[:id])
+    deletedUser.destroy
+    flash[:success] = "User #{deletedUser.name} destroyed"
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+	params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :encrypted_password)
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
+  end
+
+end
