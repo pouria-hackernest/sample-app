5807eb70034f3a02881092433be6071ce5c81a21
pouria-hackernest <pouria@hackernest.com>
2015-08-20 19:56:45 -0400

    Changed db path

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ role :db,  "192.168.1.103"
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
-after 'deploy:update_code', 'deploy:migrate'
+#after 'deploy:update_code', 'deploy:migrate'
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
