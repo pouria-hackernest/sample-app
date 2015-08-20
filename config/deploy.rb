820ff46868be0dfa6b70883a69c1747eed4865fa
pouria-hackernest <pouria@hackernest.com>
2015-08-20 19:13:33 -0400

    Put everything in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,13 @@
+require "bundler/capistrano"
+
 set :application, "sample app"
 set :scm, :git
 set :repository,  "git@github.com:pouria-hackernest/sample-app.git"
 set :scm_passphrase, ""
+set :deploy_to, "/root/deploy"
+
+set :scm_username , "pouria-hackernest"
+set :branch, fetch(:branch, "master")
 
 set :user, "root"
 
@@ -13,11 +19,12 @@ role :app, "192.168.1.103"                          # This may be the same as yo
 role :db,  "192.168.1.103", :primary => true # This is where Rails migrations will run
 role :db,  "192.168.1.103"
 
-set :stages, ["staging", "production"]
-set :default_stage, "production"
+#set :stages, ["staging", "production"]
+#set :default_stage, "production"
 
 # if you want to clean up old releases on each deploy uncomment this:
- after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
+after 'deploy:update_code', 'deploy:migrate'
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
