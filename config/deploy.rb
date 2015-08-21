681f83f0592b7af7b4fc0e5b9aa1e17d25c42c70
pouria-hackernest <pouria@hackernest.com>
2015-08-21 12:23:07 -0400

    changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,34 +19,14 @@ set :user, "root"
 role :web, "192.168.1.103"                          # Your HTTP server, Apache/etc
 role :app, "192.168.1.103"                          # This may be the same as your `Web` server
 role :db,  "192.168.1.103", :primary => true # This is where Rails migrations will run
-role :db,  "192.168.1.103"
-
-set :mysql_params, "-u root -ppassword"
-set :mysql_db_name, "sample-app_development"
-
-after :deploy, :migrate
-desc "migrate database on server"
-task :migrate do
-  run "touch #{shared_path}/migration.list ;
-ls -1v #{current_path}/sql/*.sql 2>/dev/null > #{shared_path}/migration.available;
-diff #{shared_path}/migration.available #{shared_path}/migration.list | awk \"/^</ {print \\$2}\" | while read f ;
-do echo \"migrating $(basename $f)\"; mysql #{mysql_params} #{mysql_db_name} < $f && echo $f >> #{shared_path}/migration.list ; done;
-rm -f #{shared_path}/migration.available"
-end
-
-after "deploy:setup", :create_db
-desc "create database on server"
-task :create_db do
-  run "mysql #{mysql_params} -e \"CREATE DATABASE #{mysql_db_name}\""
-end
-
+#role :db,  "192.168.1.103"
 
 #set :stages, ["staging", "production"]
 #set :default_stage, "production"
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
-#after 'deploy:update_code', 'deploy:migrate'
+after 'deploy:bundle_install', 'deploy:migrate'
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
