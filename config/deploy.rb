983a045bd350a5264398207fd9c6fe2972415c87
pouria-hackernest <pouria@hackernest.com>
2015-08-21 12:33:28 -0400

    Creating dump and loading it in deployed server

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,12 +21,35 @@ role :app, "192.168.1.103"                          # This may be the same as yo
 role :db,  "192.168.1.103", :primary => true # This is where Rails migrations will run
 #role :db,  "192.168.1.103"
 
+
+set :local_database, "sample-app_development"
+set :local_database_user, "root"
+set :local_database_password, "password"
+set :remote_database, "sample-app_development"
+set :remote_database_user, "root"
+set :remote_database_password, "password"
+ 
+after "deploy:finalize_update", :update_database
+ 
+desc "Upload the database to the server"
+task :update_database, :roles => :db, :only => { :primary => true } do
+    filename = "dump.#{Time.now.strftime '%Y%m%dT%H%M%S'}.sql"
+    remote_path = "tmp/#{filename}"
+    on_rollback {
+        delete remote_path
+    }
+    dumped_sql = `mysqldump --user=#{local_database_user} --password=#{local_database_password} #{local_database}`
+    put dumped_sql, remote_path
+    run "mysql --user=#{remote_database_user} --password=#{remote_database_password} #{remote_database} < #{remote_path}"
+end
+
+
 #set :stages, ["staging", "production"]
 #set :default_stage, "production"
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
-after 'deploy:bundle_install', 'deploy:migrate'
+#after 'deploy:bundle_install', 'deploy:migrate'
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
