27ba8032a4a3d8498a189d6bf7abe32482282146
pouria-hackernest <pouria@hackernest.com>
2015-08-21 12:15:06 -0400

    Changed config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,26 @@ role :app, "192.168.1.103"                          # This may be the same as yo
 role :db,  "192.168.1.103", :primary => true # This is where Rails migrations will run
 role :db,  "192.168.1.103"
 
+set :mysql_params, "-u root -ppassword"
+set :mysql_db_name, "sample-app_development"
+
+after :deploy, :migrate
+desc "migrate database on server"
+task :migrate do
+  run "touch #{shared_path}/migration.list ;
+ls -1v #{current_path}/sql/*.sql 2>/dev/null > #{shared_path}/migration.available;
+diff #{shared_path}/migration.available #{shared_path}/migration.list | awk \"/^</ {print \\$2}\" | while read f ;
+do echo \"migrating $(basename $f)\"; mysql #{mysql_params} #{mysql_db_name} < $f && echo $f >> #{shared_path}/migration.list ; done;
+rm -f #{shared_path}/migration.available"
+end
+
+after "deploy:setup", :create_db
+desc "create database on server"
+task :create_db do
+  run "mysql #{mysql_params} -e \"CREATE DATABASE #{mysql_db_name}\""
+end
+
+
 #set :stages, ["staging", "production"]
 #set :default_stage, "production"
 
